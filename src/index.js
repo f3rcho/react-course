ede7b02c7d5f412366892fa7b7641dbc71dba376
Fernando Cordero <fernan942@gmail.com>
2020-06-13 19:57:52 -0400

    WebPack: packing modules 10

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-// import App from './App';
-// import HelloWorld from './components/HelloWorld';
-import Button from './components/Button';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import HelloWorld from './components/HelloWorld'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Button text="Click 1"/>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <React.StrictMode>
+        <HelloWorld/>
+    </React.StrictMode>, document.getElementById(app)
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
