93e370f9798de3608cb931a090cee7f94f705c23
Fernando Cordero <fernan942@gmail.com>
2020-06-14 14:50:46 -0400

    Header structure 15

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import HelloWorld from './components/HelloWorld'
+import App from './containers/App'
 
 ReactDOM.render(
     <React.StrictMode>
-        <HelloWorld/>
+        <App/>
     </React.StrictMode>, document.getElementById('app')
 )
 
