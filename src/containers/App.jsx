7ba245334e76666cfa73bdffa672d5c7da7a4529
Fernando Cordero <fernan942@gmail.com>
2020-06-15 23:21:33 -0400

    React Hooks: useEffect and useState 23

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories'
@@ -8,56 +8,69 @@ import Footer from '../components/Footer'
 
 import '../assets/styles/App.scss';
 
-const App = () => (
-    <div className="App">
-        <Header/>
-        <Search/>
+const App = () => {
+    const [ videos, setVideos ] = useState([]);
+    const API = 'http://localhost:3000/initalState';
+    useEffect(() => {
+        fetch(API)
+            .then(response => response.json())
+            .then(data => setVideos(data))
+    }, []);
 
-        <Categories title="My list">
-            <Carousel>
-                <CarouselItem url="https://s3.amazonaws.com/mundo-bucket-s3/wp-content/uploads/2018/01/07123638/jenselter4.jpg"/>
-                <CarouselItem url="https://s3.amazonaws.com/mundo-bucket-s3/wp-content/uploads/2018/01/07123638/jenselter4.jpg"/>
-                <CarouselItem url="https://s3.amazonaws.com/mundo-bucket-s3/wp-content/uploads/2018/01/07123638/jenselter4.jpg"/>
-                <CarouselItem url="https://s3.amazonaws.com/mundo-bucket-s3/wp-content/uploads/2018/01/07123638/jenselter4.jpg"/>
-                <CarouselItem url="https://s3.amazonaws.com/mundo-bucket-s3/wp-content/uploads/2018/01/07123638/jenselter4.jpg"/>
-                <CarouselItem url="https://s3.amazonaws.com/mundo-bucket-s3/wp-content/uploads/2018/01/07123638/jenselter4.jpg"/>
-                <CarouselItem url="https://s3.amazonaws.com/mundo-bucket-s3/wp-content/uploads/2018/01/07123638/jenselter4.jpg"/>
-                <CarouselItem url="https://s3.amazonaws.com/mundo-bucket-s3/wp-content/uploads/2018/01/07123638/jenselter4.jpg"/>
+    console.log(videos);
 
-            </Carousel>
-        </Categories>
 
-        <Categories title="Trens">
-            <Carousel>
-                <CarouselItem url="https://ejerciciosencasa.es/wp-content/uploads/2014/04/jen-selter-la-mejor-cola-de-instagram9oki.jpg"/>
-                <CarouselItem url="https://ejerciciosencasa.es/wp-content/uploads/2014/04/jen-selter-la-mejor-cola-de-instagram9oki.jpg"/>
-                <CarouselItem url="https://ejerciciosencasa.es/wp-content/uploads/2014/04/jen-selter-la-mejor-cola-de-instagram9oki.jpg"/>
-                <CarouselItem url="https://ejerciciosencasa.es/wp-content/uploads/2014/04/jen-selter-la-mejor-cola-de-instagram9oki.jpg"/>
-                <CarouselItem url="https://ejerciciosencasa.es/wp-content/uploads/2014/04/jen-selter-la-mejor-cola-de-instagram9oki.jpg"/>
-                <CarouselItem url="https://ejerciciosencasa.es/wp-content/uploads/2014/04/jen-selter-la-mejor-cola-de-instagram9oki.jpg"/>
-                <CarouselItem url="https://ejerciciosencasa.es/wp-content/uploads/2014/04/jen-selter-la-mejor-cola-de-instagram9oki.jpg"/>
-                <CarouselItem url="https://ejerciciosencasa.es/wp-content/uploads/2014/04/jen-selter-la-mejor-cola-de-instagram9oki.jpg"/>
+    return (
+        <div className="App">
+            <Header/>
+            <Search/>
 
-            </Carousel>
-        </Categories>
+            <Categories title="My list">
+                <Carousel>
+                    <CarouselItem url="https://s3.amazonaws.com/mundo-bucket-s3/wp-content/uploads/2018/01/07123638/jenselter4.jpg"/>
+                    <CarouselItem url="https://s3.amazonaws.com/mundo-bucket-s3/wp-content/uploads/2018/01/07123638/jenselter4.jpg"/>
+                    <CarouselItem url="https://s3.amazonaws.com/mundo-bucket-s3/wp-content/uploads/2018/01/07123638/jenselter4.jpg"/>
+                    <CarouselItem url="https://s3.amazonaws.com/mundo-bucket-s3/wp-content/uploads/2018/01/07123638/jenselter4.jpg"/>
+                    <CarouselItem url="https://s3.amazonaws.com/mundo-bucket-s3/wp-content/uploads/2018/01/07123638/jenselter4.jpg"/>
+                    <CarouselItem url="https://s3.amazonaws.com/mundo-bucket-s3/wp-content/uploads/2018/01/07123638/jenselter4.jpg"/>
+                    <CarouselItem url="https://s3.amazonaws.com/mundo-bucket-s3/wp-content/uploads/2018/01/07123638/jenselter4.jpg"/>
+                    <CarouselItem url="https://s3.amazonaws.com/mundo-bucket-s3/wp-content/uploads/2018/01/07123638/jenselter4.jpg"/>
 
-        <Categories title="Original from PlatziVideo">
-            <Carousel>
-                <CarouselItem url="https://pbs.twimg.com/media/BjM30DbIYAAjGWs.jpg"/>
-                <CarouselItem url="https://pbs.twimg.com/media/BjM30DbIYAAjGWs.jpg"/>
-                <CarouselItem url="https://pbs.twimg.com/media/BjM30DbIYAAjGWs.jpg"/>
-                <CarouselItem url="https://pbs.twimg.com/media/BjM30DbIYAAjGWs.jpg"/>
-                <CarouselItem url="https://pbs.twimg.com/media/BjM30DbIYAAjGWs.jpg"/>
-                <CarouselItem url="https://pbs.twimg.com/media/BjM30DbIYAAjGWs.jpg"/>
-                <CarouselItem url="https://pbs.twimg.com/media/BjM30DbIYAAjGWs.jpg"/>
-                <CarouselItem url="https://pbs.twimg.com/media/BjM30DbIYAAjGWs.jpg"/>
+                </Carousel>
+            </Categories>
 
-            </Carousel>
-        </Categories>
+            <Categories title="Trens">
+                <Carousel>
+                    <CarouselItem url="https://ejerciciosencasa.es/wp-content/uploads/2014/04/jen-selter-la-mejor-cola-de-instagram9oki.jpg"/>
+                    <CarouselItem url="https://ejerciciosencasa.es/wp-content/uploads/2014/04/jen-selter-la-mejor-cola-de-instagram9oki.jpg"/>
+                    <CarouselItem url="https://ejerciciosencasa.es/wp-content/uploads/2014/04/jen-selter-la-mejor-cola-de-instagram9oki.jpg"/>
+                    <CarouselItem url="https://ejerciciosencasa.es/wp-content/uploads/2014/04/jen-selter-la-mejor-cola-de-instagram9oki.jpg"/>
+                    <CarouselItem url="https://ejerciciosencasa.es/wp-content/uploads/2014/04/jen-selter-la-mejor-cola-de-instagram9oki.jpg"/>
+                    <CarouselItem url="https://ejerciciosencasa.es/wp-content/uploads/2014/04/jen-selter-la-mejor-cola-de-instagram9oki.jpg"/>
+                    <CarouselItem url="https://ejerciciosencasa.es/wp-content/uploads/2014/04/jen-selter-la-mejor-cola-de-instagram9oki.jpg"/>
+                    <CarouselItem url="https://ejerciciosencasa.es/wp-content/uploads/2014/04/jen-selter-la-mejor-cola-de-instagram9oki.jpg"/>
 
-        <Footer/>
-    </div>
-);
+                </Carousel>
+            </Categories>
+
+            <Categories title="Original from PlatziVideo">
+                <Carousel>
+                    <CarouselItem url="https://pbs.twimg.com/media/BjM30DbIYAAjGWs.jpg"/>
+                    <CarouselItem url="https://pbs.twimg.com/media/BjM30DbIYAAjGWs.jpg"/>
+                    <CarouselItem url="https://pbs.twimg.com/media/BjM30DbIYAAjGWs.jpg"/>
+                    <CarouselItem url="https://pbs.twimg.com/media/BjM30DbIYAAjGWs.jpg"/>
+                    <CarouselItem url="https://pbs.twimg.com/media/BjM30DbIYAAjGWs.jpg"/>
+                    <CarouselItem url="https://pbs.twimg.com/media/BjM30DbIYAAjGWs.jpg"/>
+                    <CarouselItem url="https://pbs.twimg.com/media/BjM30DbIYAAjGWs.jpg"/>
+                    <CarouselItem url="https://pbs.twimg.com/media/BjM30DbIYAAjGWs.jpg"/>
+
+                </Carousel>
+            </Categories>
+
+            <Footer/>
+        </div>
+    )
+}
 
 
 export default App;
