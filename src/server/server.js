01a1664d6cb3795f59fd3b6aafc7965e51afba98
Fernando Cordero <fernan942@gmail.com>
2020-07-04 19:31:00 -0400

    Using Nodemon and dotenv 6

--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const { ENV, PORT } = process.env;
+
+if (ENV === 'development') {
+    console.log('Development config');
+}
+
 const app = express();
 
 app.get('*', (req, res) => {
     res.send({ hello: 'Hello', world: 'World'});
 });
 
-const server = app.listen(3000, () => {
+const server = app.listen(PORT, () => {
     console.log(`Server listen on port http://localhost:${server.address().port}`);
 });
