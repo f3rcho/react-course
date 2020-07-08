9af732f44ebfe233554975a948e52bf466821f42
Fernando Cordero <fernan942@gmail.com>
2020-07-08 19:48:08 -0400

    Webpack Integration with Express 7

--- src/server/server.js
+++ src/server/server.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const dotenv = require('dotenv');
+import webpack from 'webpack';
 
 dotenv.config();
 
 const { ENV, PORT } = process.env;
+const app = express();
 
 if (ENV === 'development') {
     console.log('Development config');
+    const webpackConfig = require('../../webpack.config');
+    const webpackDevMiddleware = require('webpack-dev-middleware');
+    const webpackHotMiddleware = require('webpack-hot-middleware');
+    const compiler = webpack(webpackConfig);
+    const serverConfig = { port: PORT, hot: true };
+
+    app.use(webpackDevMiddleware(compiler, serverConfig));
+    app.use(webpackHotMiddleware(compiler));
 }
 
-const app = express();
 
 app.get('*', (req, res) => {
     res.send({ hello: 'Hello', world: 'World'});
