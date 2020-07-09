9060a76d99b3a427e17da60f01efccaf18087f9e
Fernando Cordero <fernan942@gmail.com>
2020-07-08 20:43:20 -0400

    Serving React with Express 8

--- src/server/server.js
+++ src/server/server.js
@@ -21,9 +21,21 @@ if (ENV === 'development') {
 
 
 app.get('*', (req, res) => {
-    res.send({ hello: 'Hello', world: 'World'});
+    res.send( `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="assets/app.css" type="text/css">
+        <title>Platzi Video</title>
+    </head>
+    <body>
+        <div id="app"></div>
+        <script src="assets/app.js" type="text/javascript"></script>
+    </body>
+    </html>`);
 });
 
 const server = app.listen(PORT, () => {
-    console.log(`Server listen on port http://localhost:${server.address().port}`);
+        console.log(`Server listen on port http://localhost:${server.address().PORT}`);
 });
