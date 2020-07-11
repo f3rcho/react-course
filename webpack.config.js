746051d703aa49e1600d6dfdeb0e1ed7c825cfd3
Fernando Cordero <fernan942@gmail.com>
2020-07-11 17:55:50 -0400

    Setting webpack up for production 14

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,23 @@ const path = require('path');
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
+require('dotenv').config();
+
+// development condition
+const isDev = (process.env.ENV === 'development');
+// entry for productioin mode
+const entry = ['./src/frontend/index.js'];
+// entry for development mode
+if (isDev) {
+    entry.push('webpack-hot-middleware/client?path=/__webpack_hmr&timeout=2000$reload=true')
+};
+
+
 module.exports = {
-    entry: ['./src/frontend/index.js', 'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=2000$reload=true'],
-    mode: 'development',
+    entry,
+    mode: process.env.ENV,
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'src/server/public'),
         filename: 'assets/app.js',
         publicPath:'/',
     },
@@ -62,7 +74,7 @@ module.exports = {
         historyApiFallback: true,
     },
     plugins: [
-        new webpack.HotModuleReplacementPlugin(),
+        isDev ? new webpack.HotModuleReplacementPlugin() : ()=> {},
         new MiniCssExtractPlugin({
             filename: 'assets/app.css'
         }),
